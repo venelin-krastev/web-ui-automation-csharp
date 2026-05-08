49d089c96e78c15ac7b52ce46466770c89aeb268
Venelin Krustev <venelin.v.krustev@gmail.com>
2026-05-08 18:25:30 +0300

    refactor: apply Page Object Model pattern

---
+++ Pages/GooglePage.cs
@@ -0,0 +1,41 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace SeleniumBasics.Pages;
+
+public class GooglePage
+{
+    private readonly IWebDriver driver;
+    private readonly WebDriverWait wait;
+
+    public GooglePage(IWebDriver driver)
+    {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+    }
+
+    public void Open()
+    {
+        driver.Navigate().GoToUrl("https://www.google.com");
+    }
+
+    public string GetTitle()
+    {
+        return driver.Title;
+    }
+
+    public void Search(string query)
+    {
+        IWebElement searchBox = wait.Until(d =>
+        {
+            var el = d.FindElement(By.CssSelector("textarea[name='q']"));
+            return el.Displayed && el.Enabled ? el : null;
+        });
+
+        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+        js.ExecuteScript("arguments[0].value='" + query + "';", searchBox);
+        searchBox.Submit();
+
+        wait.Until(d => d.Title.Contains(query.Split(' ')[0]));
+    }
+}
--- UnitTest1.cs
+++ UnitTest1.cs
@@ -1,40 +1,33 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using OpenQA.Selenium.Support.UI;
+using SeleniumBasics.Pages;
 
 namespace SeleniumBasics;
 
 public class Tests
 {
     private IWebDriver driver;
+    private GooglePage googlePage;
 
     [SetUp]
     public void Setup()
     {
         driver = new ChromeDriver();
+        googlePage = new GooglePage(driver);
     }
 
     [Test]
     public void GoogleTitleIsCorrect()
     {
-        driver.Navigate().GoToUrl("https://www.google.com");
-        string title = driver.Title;
-        Assert.That(title, Does.Contain("Google"));
+        googlePage.Open();
+        Assert.That(googlePage.GetTitle(), Does.Contain("Google"));
     }
 
     [Test]
     public void GoogleSearchReturnsResults()
     {
-        driver.Navigate().GoToUrl("https://www.google.com");
-        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-        IWebElement searchBox = wait.Until(d => {
-            var el = d.FindElement(By.CssSelector("textarea[name='q']"));
-            return el.Displayed && el.Enabled ? el : null;
-        });
-        IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
-        js.ExecuteScript("arguments[0].value='Selenium C#';", searchBox);
-        searchBox.Submit();
-        wait.Until(d => d.Title.Contains("Selenium"));
+        googlePage.Open();
+        googlePage.Search("Selenium C#");
         Assert.That(driver.Title, Does.Contain("Selenium"));
     }
 
